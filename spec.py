e5c0d446a42015f6192d8d43cdc94c6a7409cffc
Tobin Ehlis <tobine@google.com>
2016-09-29 14:51:52 -0600

    WIP : Re-wrote spec parser to avoid duplicates
    Added 1600 spec as a temporary convenience
    
    Added help to the spec.py script

--- spec.py
+++ spec.py
@@ -14,6 +14,15 @@ spec_url = "https://www.khronos.org/registry/vulkan/specs/1.0/xhtml/vkspec.html"
 error_msg_prefix = "For more information refer to Vulkan Spec Section"
 ns = {'ns': 'http://www.w3.org/1999/xhtml'}
 
+def printHelp():
+    print "Usage: python spec.py [-spec <specfile.html>] [-out <headerfile.h>] [-gendb <databasefile.txt>] [-compare <databasefile.txt>] [-help]"
+    print "\n Default script behavior is to parse the specfile and generate a header of unique error enums and corresponding error messages based on the specfile.\n"
+    print "  Default specfile is vkspec.html"
+    print "  Default headerfile vk_validation_error_messages.h"
+    print "  Default databasefile is vk_validation_error_database.txt"
+    print "\nIf '-gendb' option is specified then a database file is generated that stores the list of enums and their error messages"
+    print "\nIf '-compare' option is specified then the given database file will be read in as the baseline for generating the new specfile"
+
 class Specification:
     def __init__(self):
         self.tree   = None
@@ -31,46 +40,40 @@ class Specification:
     def parseTree(self):
         """Parse the registry Element, once created"""
         print "Parsing spec file..."
-        # This must be the Element for the root <registry>
         valid_usage = False # are we under a valid usage branch?
-        section_heading = '' # the text heading for the section
-        section_link = '' # txt for the section html link
         unique_enum_id = 0
         self.root = self.tree.getroot()
         #print "ROOT: %s" % self.root
-        #for child in self.root:
-        #    print child.tag, child.attrib
-        for div in self.root.iter('{http://www.w3.org/1999/xhtml}div'):
-            # we're under a section
-            #print "div: %s" % (div.tag)
-            if div.get('class') == 'section':
-                #print "section: %s" % (div.attrib)
-                # Find section headings and link
-                for h2 in div.iter('{http://www.w3.org/1999/xhtml}h2'):
-                    if h2.get('class') == 'title':
-                        #print "Title: %s" % (h2)
-                        section_heading = "".join(h2.itertext())
-                        # Insert a space between heading number & title
-                        sh_list = section_heading.rsplit('.', 1)
-                        section_heading = '. '.join(sh_list)
-                        section_link = h2[0].get('id')
-                        #print "Title heading (%s) & link (%s)" % (section_heading, section_link)
-                        #return
-                for sb in div.iter('{http://www.w3.org/1999/xhtml}div'):
-                    if sb.get('class') == 'sidebar':
-                        for strong in sb.iter('{http://www.w3.org/1999/xhtml}strong'):
-                            if strong.text != None and 'Valid Usage' in strong.text:
-                                valid_usage = True
-                            else:
-                                valid_usage = False
-                        if valid_usage:
-                            for li in sb.iter('{http://www.w3.org/1999/xhtml}li'):
-                                # Here is where we need to save valid usage reqs
-                                error_msg_str = "%s '%s' which states '%s' (%s#%s)" % (error_msg_prefix, section_heading, "".join(li.itertext()).replace('\n', ''), spec_url, section_link)
-                                enum_str = "VALIDATION_ERROR_%s%d" % (section_heading.split()[0].replace('.', '_'), unique_enum_id)
-                                self.val_error_dict[enum_str.encode("ascii", "ignore")] = error_msg_str.encode("ascii", "ignore")
-                                unique_enum_id = unique_enum_id + 1
-                                #print "dict contents: %s:" % (self.val_error_dict)
+        prev_heading = '' # Last seen section heading or sub-heading
+        prev_link = '' # Last seen link id within the spec
+        for tag in self.root.iter(): # iterate down tree
+            # Grab most recent section heading and link
+            if tag.tag in ['{http://www.w3.org/1999/xhtml}h2', '{http://www.w3.org/1999/xhtml}h3']:
+                if tag.get('class') != 'title':
+                    continue
+                #print "Found heading %s" % (tag.tag)
+                prev_heading = "".join(tag.itertext())
+                # Insert a space between heading number & title
+                sh_list = prev_heading.rsplit('.', 1)
+                prev_heading = '. '.join(sh_list)
+                prev_link = tag[0].get('id')
+                #print "Set prev_heading %s to have link of %s" % (prev_heading.encode("ascii", "ignore"), prev_link.encode("ascii", "ignore"))
+            elif tag.tag == '{http://www.w3.org/1999/xhtml}a': # grab any intermediate links
+                if tag.get('id') != None:
+                    prev_link = tag.get('id')
+                    #print "Updated prev link to %s" % (prev_link)
+            elif tag.tag == '{http://www.w3.org/1999/xhtml}strong': # identify valid usage sections
+                if None != tag.text and 'Valid Usage' in tag.text:
+                    valid_usage = True
+                else:
+                    valid_usage = False
+            elif tag.tag == '{http://www.w3.org/1999/xhtml}li' and valid_usage: # grab actual valid usage requirements
+                error_msg_str = "%s '%s' which states '%s' (%s#%s)" % (error_msg_prefix, prev_heading, "".join(tag.itertext()).replace('\n', ''), spec_url, prev_link)
+                enum_str = "VALIDATION_ERROR_%d" % (unique_enum_id)
+                self.val_error_dict[enum_str.encode("ascii", "ignore")] = error_msg_str.encode("ascii", "ignore")
+                unique_enum_id = unique_enum_id + 1
+                #print "dict contents: %s:" % (self.val_error_dict)
+                #print "Added enum to dict: %s" % (enum_str.encode("ascii", "ignore"))
         #print "Validation Error Dict has a total of %d unique errors and contents are:\n%s" % (unique_enum_id, self.val_error_dict)
     def genHeader(self, header_file):
         """Generate a header file based on the contents of a parsed spec"""
@@ -154,44 +157,70 @@ class Specification:
         new_err_to_id_dict = {}
         for enum in self.val_error_dict:
             new_err_to_id_dict[self.val_error_dict[enum]] = enum
+        ids_parsed = 0
         # Now parse through new dict and figure out what to do with non-matching things
         for enum in self.val_error_dict:
+            ids_parsed = ids_parsed + 1
             enum_list = enum.split('_') # grab sections of enum for use below
             if enum in orig_db_dict:
                 if self.val_error_dict[enum] == orig_db_dict[enum]:
-                    #print "Exact match for enum %s" % (enum)
+                    print "Exact match for enum %s" % (enum)
                     # Nothing to see here
+                    if enum in updated_val_error_dict:
+                        print "ERROR: About to overwrite entry for %s" % (enum)
                     updated_val_error_dict[enum] = self.val_error_dict[enum]
                 elif self.val_error_dict[enum] in orig_err_to_id_dict:
                     # Same value w/ different error id, need to anchor to original id
-                    #print "Need to switch new id %s to original id %s" % (enum, orig_err_to_id_dict[self.val_error_dict[enum]])
+                    print "Need to switch new id %s to original id %s" % (enum, orig_err_to_id_dict[self.val_error_dict[enum]])
                     # Update id at end of new enum to be same id from original enum
                     enum_list[-1] = orig_err_to_id_dict[self.val_error_dict[enum]].split('_')[-1]
                     new_enum = "_".join(enum_list)
+                    if new_enum in updated_val_error_dict:
+                        print "ERROR: About to overwrite entry for %s" % (new_enum)
                     updated_val_error_dict[new_enum] = self.val_error_dict[enum]
                 else:
                     # Completely new id, need to pick it up from end of original unique ids
                     enum_list[-1] = str(next_id)
                     new_enum = "_".join(enum_list)
                     next_id = next_id + 1
-                    updated_val_error_dict[new_enum] = self.val_error_dict[enum]
                     print "MANUALLY VERIFY: Updated new enum %s to be unique %s. Make sure new error msg is actually unique and not just changed" % (enum, new_enum)
                     print "   New error string: %s" % (self.val_error_dict[enum])
+                    if new_enum in updated_val_error_dict:
+                        print "ERROR: About to overwrite entry for %s" % (new_enum)
+                    updated_val_error_dict[new_enum] = self.val_error_dict[enum]
             else: # new enum is not in orig db
                 if self.val_error_dict[enum] in orig_err_to_id_dict:
-                    #print "New enum %s not in orig dict, but exact error message matches original unique id %s" % (enum, orig_err_to_id_dict[self.val_error_dict[enum]])
+                    print "New enum %s not in orig dict, but exact error message matches original unique id %s" % (enum, orig_err_to_id_dict[self.val_error_dict[enum]])
                     # Update new unique_id to use original
                     enum_list[-1] = orig_err_to_id_dict[self.val_error_dict[enum]].split('_')[-1]
                     new_enum = "_".join(enum_list)
+                    if new_enum in updated_val_error_dict:
+                        print "ERROR: About to overwrite entry for %s" % (new_enum)
                     updated_val_error_dict[new_enum] = self.val_error_dict[enum]
                 else:
                     enum_list[-1] = str(next_id)
                     new_enum = "_".join(enum_list)
                     next_id = next_id + 1
-                    #print "Completely new id and error code, update new id from %s to unique %s" % (enum, new_enum)
+                    print "Completely new id and error code, update new id from %s to unique %s" % (enum, new_enum)
+                    if new_enum in updated_val_error_dict:
+                        print "ERROR: About to overwrite entry for %s" % (new_enum)
                     updated_val_error_dict[new_enum] = self.val_error_dict[enum]
         # Assign parsed dict to be the udpated dict based on db compare
+        print "In compareDB parsed %d entries" % (ids_parsed)
         return updated_val_error_dict
+    def validateUpdateDict(self, update_dict):
+        """Compare original dict vs. update dict and make sure that all of the checks are still there"""
+        # Currently just make sure that the same # of checks as the original checks are there
+        #orig_ids = {}
+        orig_id_count = len(self.val_error_dict)
+        #update_ids = {}
+        update_id_count = len(update_dict)
+        if orig_id_count != update_id_count:
+            print "Original dict had %d unique_ids, but updated dict has %d!" % (orig_id_count, update_id_count)
+            return False
+        print "Original dict and updated dict both have %d unique_ids. Great." % (orig_id_count)
+        return True
+        # TODO : include some more analysis
 
 if __name__ == "__main__":
     i = 1
@@ -212,6 +241,9 @@ if __name__ == "__main__":
             db_filename = sys.argv[i]
             spec_compare = True
             i = i + 1
+        elif (arg in ['-help', '-h']):
+            printHelp()
+            sys.exit()
     print "Using spec file (-spec) to '%s'" % (spec_filename)
     print "Writing out file (-out) to '%s'" % (out_filename)
     spec = Specification()
@@ -224,7 +256,11 @@ if __name__ == "__main__":
         (orig_db_dict, max_id) = spec.readDB(db_filename)
         # New spec data should already be read into self.val_error_dict
         updated_dict = spec.compareDB(orig_db_dict, max_id)
-        spec.updateDict(updated_dict)
+        update_valid = spec.validateUpdateDict(updated_dict)
+        if update_valid:
+            spec.updateDict(updated_dict)
+        else:
+            sys.exit()
     if (gen_db):
         spec.genDB(db_filename)
     spec.genHeader(out_filename)
@@ -285,3 +321,39 @@ if __name__ == "__main__":
         #   </ul>
         # </div>
         # </div>
+# <div class="sidebar">
+#   <div class="titlepage">
+#     <div>
+#       <div>
+#         <p class="title">
+#           <strong>Valid Usage</strong>
+#         </p>
+#       </div>
+#     </div>
+#   </div>
+#   <div class="itemizedlist">
+#     <ul class="itemizedlist" style="list-style-type: disc; ">
+#       <li class="listitem">The <em class="parameter"><code>queueFamilyIndex</code></em> member of any given element of <em class="parameter"><code>pQueueCreateInfos</code></em> <span class="normative">must</span> be unique within <em class="parameter"><code>pQueueCreateInfos</code></em>
+#       </li>
+#     </ul>
+#   </div>
+# </div>
+# <div class="sidebar">
+#   <div class="titlepage">
+#     <div>
+#       <div>
+#         <p class="title">
+#           <strong>Valid Usage (Implicit)</strong>
+#         </p>
+#       </div>
+#     </div>
+#   </div>
+#   <div class="itemizedlist"><ul class="itemizedlist" style="list-style-type: disc; "><li class="listitem">
+#<em class="parameter"><code>sType</code></em> <span class="normative">must</span> be <code class="code">VK_STRUCTURE_TYPE_DEVICE_CREATE_INFO</code>
+#</li><li class="listitem">
+#<em class="parameter"><code>pNext</code></em> <span class="normative">must</span> be <code class="literal">NULL</code>
+#</li><li class="listitem">
+#<em class="parameter"><code>flags</code></em> <span class="normative">must</span> be <code class="literal">0</code>
+#</li><li class="listitem">
+#<em class="parameter"><code>pQueueCreateInfos</code></em> <span class="normative">must</span> be a pointer to an array of <em class="parameter"><code>queueCreateInfoCount</code></em> valid <code class="code">VkDeviceQueueCreateInfo</code> structures
+#</li>
